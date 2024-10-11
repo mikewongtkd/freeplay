479618c92092f3e2bb5f44d0b28130de0f44f154
Annabella Chow <annabella.chow@berkeley.edu>
2024-10-11 09:26:26 -0700

    update layout of app

--- app.py
+++ app.py
@@ -1,85 +1,60 @@
-import sys
-from stages import overview
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+# import sys
+# from stages import overview
+# from PyQt5.QtGui import *
+# from PyQt5.QtWidgets import *
+# from PyQt5.QtCore import *
 
 
-
-class MyWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        #template/overall structure
-        self.main = QVBoxLayout()
-
-        
-        self.row1 = QHBoxLayout()
-        self.overview = overview.Overview()
-        
-    
-        #now LHS taskbar
-        self.taskbar = QVBoxLayout()
-
-
-        self.logo = QLabel()
-
-        # Load an image using QPixmap
-        pixmap = QPixmap('images/FakeLogo.png')  # Replace with your image path
-        # Resize the image using the scaled method
-        pixmap = pixmap.scaled(200, 400, aspectRatioMode=Qt.KeepAspectRatio)
-        # Set the QPixmap on the QLabel
-        self.logo.setPixmap(pixmap)
-        self.taskbar.addWidget(self.logo)
-
-        #Live/Replay Status
-        self.status = QPushButton("Live/Replay") #placeholder
-        self.taskbar.addWidget(self.status)
-
-        #Chung/Hong Marker
-        self.chlabel = QLabel("Mark Quotas")
-        self.chlabel.setAlignment(Qt.AlignCenter)
-        self.taskbar.addWidget(self.chlabel)
-
-        # self.chButton = 
-        self.chungBTM = QLabel("Chung")
-        self.hongBTM = QLabel("Hong")
-
-
-
-        self.row1.addLayout(self.taskbar)
-        self.row1.addLayout(self.overview.getLayout())
-        self.main.addLayout(self.row1)
-
-
-
-        self.setLayout(self.main)
-        
-
-
-if __name__ == "__main__":
-    
-    app = QApplication(sys.argv)
-    widget = MyWidget()
-    # widget.resize(1500, 1200)
-    widget.show()
-
-
-    sys.exit(app.exec())
 
 # class MyWidget(QWidget):
 #     def __init__(self):
 #         super().__init__()
-#         #pages
-#         setup = setup.Setup()
-#         main_page = main_page.Main_Page()
 
+#         #template/overall structure
+#         self.main = QVBoxLayout()
 
-#         state = (setup, main_page)
-#         self.page = setup
+        
+#         self.row1 = QHBoxLayout()
+#         self.overview = overview.Overview()
+        
     
+#         #now LHS taskbar
+#         self.taskbar = QVBoxLayout()
 
-    
+
+#         self.logo = QLabel()
+
+#         # Load an image using QPixmap
+#         pixmap = QPixmap('images/FakeLogo.png')  # Replace with your image path
+#         # Resize the image using the scaled method
+#         pixmap = pixmap.scaled(200, 400, aspectRatioMode=Qt.KeepAspectRatio)
+#         # Set the QPixmap on the QLabel
+#         self.logo.setPixmap(pixmap)
+#         self.taskbar.addWidget(self.logo)
+
+#         #Live/Replay Status
+#         self.status = QPushButton("Live/Replay") #placeholder
+#         self.taskbar.addWidget(self.status)
+
+#         #Chung/Hong Marker
+#         self.chlabel = QLabel("Mark Quotas")
+#         self.chlabel.setAlignment(Qt.AlignCenter)
+#         self.taskbar.addWidget(self.chlabel)
+
+#         # self.chButton = 
+#         self.chungBTM = QLabel("Chung")
+#         self.hongBTM = QLabel("Hong")
+
+
+
+#         self.row1.addLayout(self.taskbar)
+#         self.row1.addLayout(self.overview.getLayout())
+#         self.main.addLayout(self.row1)
+
+
+
+#         self.setLayout(self.main)
+        
 
 
 # if __name__ == "__main__":
@@ -91,3 +66,72 @@ if __name__ == "__main__":
 
 
 #     sys.exit(app.exec())
+
+
+
+import sys
+from stages import *
+# from camera import connection
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication, QStackedWidget
+from PyQt5.QtCore import *
+
+
+class App(QApplication):
+    def __init__(self, sys_argv):
+        super().__init__(sys_argv)
+        
+        
+        # Create the page controller with signals
+        # self.controller = PageController()
+
+        # Create the page manager (QStackedWidget is good for switching between pages)
+        self.page_manager = QStackedWidget()
+
+        # Initialize pages
+        self.setup = Setup()
+        # self.overview = self.init_overview()
+
+        # Add pages to the page manager
+        self.page_manager.addWidget(self.setup)
+        # self.page_manager.addWidget(self.overview)
+
+        # Show the setup page first
+        self.page_manager.setCurrentWidget(self.setup)
+        # self.page_manager.setCurrentWidget(self.overview)
+
+        # Listen for signal from one page to another
+        # self.controller.show_overview.connect(self.switch_to_overview)
+        # self.controller.show_setup.connect(self.switch_to_setup) 
+        
+
+        self.page_manager.show()
+
+    
+
+    # def init_overview(self):
+    #     #connect cameras
+    #     self.cameras_connection = connection.Connection()
+    #     self.overview = Overview(self.cameras_connection)
+        
+    # def init_single_camera_page(self, cam_num):
+    #     pass
+
+
+    # # @Slot()
+    # def switch_to_overview(self):
+    #     """Switch to the main page when the signal is received."""
+    #     self.page_manager.setCurrentWidget(self.overview)
+    
+    # # @Slot()
+    # def switch_to_setup(self):
+    #     """Switch to the main page when the signal is received."""
+    #     self.page_manager.setCurrentWidget(self.setup)
+
+
+if __name__ == "__main__":
+    app = App(sys.argv)
+    # app.resize(1500, 1200)
+
+
+    sys.exit(app.exec())
--- stages/setup.py
+++ stages/setup.py
@@ -1,11 +1,60 @@
+# import sys
+# from PyQt5.QtGui import *
+# from PyQt5.QtWidgets import *
+# from PyQt5.QtCore import *
+
+
+
+# class Setup(QWidget):
+#     def __init__(self):
+#         super().__init__()
+
+#         #template/overall structure
+#         self.main = QVBoxLayout()
+
+        
+#         self.logo = QLabel()
+
+#         # Load an image using QPixmap
+#         pixmap = QPixmap('images/FakeLogo.png')  # Replace with your image path
+#         # Resize the image using the scaled method
+#         pixmap = pixmap.scaled(400, 400, aspectRatioMode=Qt.KeepAspectRatio)
+#         # Set the QPixmap on the QLabel
+#         self.logo.setPixmap(pixmap)
+#         self.main.addWidget(self.logo)
+
+        
+#         self.start_button = QPushButton("Start", self)
+#         self.main.addWidget(self.start_button)
+
+#         self.setLayout(self.main)
+        
+
+
+# if __name__ == "__main__":
+    
+#     app = QApplication(sys.argv)
+#     widget = Setup()
+#     # widget.resize(1500, 1200)
+#     widget.show()
+
+
+#     sys.exit(app.exec())
+
+
+
+
+
 import sys
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+from PyQt5.QtCore import Qt
 
 
 
 class Setup(QWidget):
+    # Signal to inform app.py to switch pages
+    # start_signal = pyqtSignal()
     def __init__(self):
         super().__init__()
 
@@ -26,17 +75,7 @@ class Setup(QWidget):
         
         self.start_button = QPushButton("Start", self)
         self.main.addWidget(self.start_button)
+        # Connect the button click to the method that emits the signal
+        # self.start_button.clicked.connect(controller.show_overview.emit)
 
         self.setLayout(self.main)
-        
-
-
-if __name__ == "__main__":
-    
-    app = QApplication(sys.argv)
-    widget = Setup()
-    # widget.resize(1500, 1200)
-    widget.show()
-
-
-    sys.exit(app.exec())
