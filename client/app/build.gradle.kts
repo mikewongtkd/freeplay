eb6b92b5a5f55710e8aa8733b5ca7fa4bdf06c44
Mike Wong <mikewongtkd@gmail.com>
2026-08-31 22:40:57 -0700

    Apparently build fixes broke the build. Let's try this again.

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -7,16 +7,12 @@ plugins {
 
 android {
     namespace = "net.opentkd.freeplay"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "net.opentkd.freeplay"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
