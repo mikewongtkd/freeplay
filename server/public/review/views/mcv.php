8f5671e33e45cf909917139e6891bd3483c4e545
Mike Wong <mikewongtkd@gmail.com>
2026-09-14 17:34:26 -0700

    Formatting fix

--- server/public/review/views/mcv.php
+++ server/public/review/views/mcv.php
@@ -1,6 +1,6 @@
 <section id="mcvView" class="view-panel" aria-label="Multi Camera View">
-  <div class="row">
-    <div class="mcv-grid col-lg-10 col-sm-12">
+  <div class="row g-1 h-100 m-0 mcv-layout">
+    <div class="mcv-grid col-lg-10 col-12">
       <?php for ($camera = 1; $camera <= 3; $camera++): ?>
         <article class="video-tile camera-tile" data-camera-tile="<?= $camera ?>" tabindex="0" role="button" aria-label="Open Camera <?= $camera ?> in Single Camera View">
           <div class="simulated-video camera-scene camera-scene-<?= $camera ?>">
@@ -14,7 +14,7 @@
       <?php endfor; ?>
       <?php require __DIR__ . '/review-panel.php'; ?>
     </div>
-    <div class="col-lg-2 col-sm-12">
+    <div class="col-lg-2 col-12 mcv-summary-column">
       <?php require __DIR__ . '/review-summary.php'; ?>
     </div>
   </div>
